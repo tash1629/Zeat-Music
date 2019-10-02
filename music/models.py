a3bd1b6da2e241b96788533ab1d6cb6ee1b8e84c
tash1629 <rushnan.99@gmail.com>
2019-10-02 12:52:16 -0700

    Initial Commit

--- music/models.py
+++ music/models.py
@@ -5,7 +5,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField(max_length=1000)
     # anytime a change is made to a model attribute
     # make sure to, python manage.py makemigrations
     # then, python manage.py migrate
